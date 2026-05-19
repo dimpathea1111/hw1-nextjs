27d567d7f68a8d3747292b7218591e4222ca2902
pathea <pathea.dim@gmail.com>
2026-05-19 07:41:11 +0700

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import HeroSection from "./component/Navigation/hero-section";
+import ProductCard from "./component/Navigation/card-product";
+import { CardSection } from "./component/Navigation/card-section";
 
 export default function Home() {
   return (
    <div className="bg-yellow-400">
-    <h1>Hellow kon kon </h1>
+       <HeroSection/>
+       < ProductCard/>   
+
+       <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <CardSection
+       </main>
+
     <center className="font-2xl">Welcom to our website</center>
    </div>
   );
