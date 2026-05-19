2e3968743547a62d0260ca70a55a3ab572090b9d
pathea <pathea.dim@gmail.com>
2026-05-19 21:59:18 +0700

    add

---
+++ src/app/component/Navigation/card-hw.tsx
@@ -0,0 +1,11 @@
+
+
+export default function CardHW(){
+    return(
+        
+        <section className=" p-y grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div className="p-4 border border-2 bg-gray-300 "></div>
+        </section>
+
+    )
+}
---
+++ src/app/components/blogcomponent.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+interface BlogType {
+    id: number;
+    title: string;
+    body: string;
+}
+
+export default function BlogComponent({ id, title, body }: BlogType) {
+    return (
+        <div className="relative bg-gray-50 px-6 pt-16 pb-20 lg:px-8 lg:pt-24 lg:pb-28">
+            <Link href={`/blog/${id}`}>
+                <div className="absolute inset-0">
+                    <div className="h-1/3 bg-white sm:h-2/3" />
+                </div>
+            </Link>
+
+            <div className="relative mx-auto max-w-7xl text-center">
+                <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
+                    {title}
+                </h2>
+                import Image from "next/image";
+
+                <Image
+                    src="https://dummyjson.com/image.jpg"
+                    alt="blog image"
+                    width={500}
+                    height={300}
+                />
+
+                <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-500 sm:mt-4">
+                    {body}
+                </p>
+            </div>
+        </div>
+    );
+}
